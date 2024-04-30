8f00da6f0f0b00fda779df23e93d8788f0060325
CodeRadu <47945947+CodeRadu@users.noreply.github.com>
2024-04-30 20:01:04 +0300

    Fix initrd

--- post-install.sh
+++ post-install.sh
@@ -4,4 +4,7 @@ set -ouex pipefail
 
 sed -i 's/Fedora Linux/Ostree Test/g' /etc/os-release
 
-rpm-ostree initramfs --enable # Enable local initramfs generation
+KERNEL_VERSION=$(rpm -q kernel --qf "%{VERSION}-%{RELEASE}.%{ARCH}")
+dracut --no-hostonly --kver $KERNEL_VERSION -f /lib/modules/$KERNEL_VERSION/initramfs.img
+
+chmod 600 /lib/modules/$KERNEL_VERSION/initramfs.img
