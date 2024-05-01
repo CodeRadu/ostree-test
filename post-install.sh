05831c7489b14e6d9f868ccd45310f60a6a46635
CodeRadu <47945947+CodeRadu@users.noreply.github.com>
2024-05-01 12:10:02 +0300

    Fix initrd 2

--- post-install.sh
+++ post-install.sh
@@ -5,6 +5,6 @@ set -ouex pipefail
 sed -i 's/Fedora Linux/Ostree Test/g' /etc/os-release
 
 KERNEL_VERSION=$(rpm -q kernel --qf "%{VERSION}-%{RELEASE}.%{ARCH}")
-dracut --no-hostonly --kver $KERNEL_VERSION -f /lib/modules/$KERNEL_VERSION/initramfs.img
+dracut --no-hostonly --kver $KERNEL_VERSION --reproducible -v --add ostree -f /lib/modules/$KERNEL_VERSION/initramfs.img
 
 chmod 600 /lib/modules/$KERNEL_VERSION/initramfs.img
